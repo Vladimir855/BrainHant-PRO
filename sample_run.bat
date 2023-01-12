b5d1d1d26023595ec391e811915fa296d10753e0
Noname400 <74019756+Noname400@users.noreply.github.com>
2023-01-12 23:40:58 +0500

    BrainHunt Classic 3.05/12.01.23

--- sample_run.bat
+++ sample_run.bat
@@ -1,3 +1,3 @@
-rem python -B BrainHunt.py -th 3 -id 0 -dbbtc BF/btc.bf -dbalt BF/alt.bf -dbeth BF/eth.bf -in dictionaries/out.txt -telegram -desc home -save 30 -minout
-python -B BrainHunt.py -th 1 -id 0 -dbbtc btc.bin -dbalt btc.bin -dbeth btc.bin -in dictionaries/hex_test.txt -desc home -save 3000 -minout
+rem python -B BrainHunt.py -th 3 -id 0 -bf_dir -in dictionaries/out.txt -telegram -desc home -save 30 -minout
+python -B BrainHunt.py -th 1 -id 0 -dbdir D:\GitHub\BrainHant-PRO\BF -in dictionaries/hash.txt -telegram -raw -desc home -save 3000 -minout
 pause
